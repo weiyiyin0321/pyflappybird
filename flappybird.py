d7162f1266c0d2a5cd9130fc6383128ad3acbc71
Yin, Weiyi <weiyi.yin@level3.com>
2019-01-11 12:23:33 -0700

    made game into a class

---
+++ flappybird.py
@@ -0,0 +1,107 @@
+import pygame, sys
+import numpy as np
+from game_elements import Pipes, Bird
+from game_mechanics import Score, BirdPipeRelation, convert_pipes_group_to_pipe_rect, convert_pipes_group_to_pipe_rect_pairs
+from game_memory import create_state, create_memory
+import time
+
+class FlappyBird:
+
+    def __init__(self):
+
+        pygame.init()
+        self.background = pygame.image.load('./images/background.jpg')
+        self.screen_x = 640
+        self.screen_y = 480
+        self.gap_width = 0.3
+        self.pipe_width = 0.1
+        self.space_between_pipes = 3 # a multiple of the pipe width
+        self.screen = pygame.display.set_mode((self.screen_x, self.screen_y))
+        self.pipes_group = pygame.sprite.Group()
+        self.bird = Bird(screen_x = self.screen_x, screen_y = self.screen_y, gravity = 1.15)
+        self.bird.create_bird_rect(start_x = 0.5, start_y = 0.2, size_x = 44, size_y = 30)
+        self.t=0
+        self.jump_velocity = -14
+        self.score = 0
+        self.scoring = Score()
+
+    def play(self):
+        while True:
+            self.run_frame()
+    
+    def run_frame(self, action=False):
+        self.t = self.t+1
+        self.action = 0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    action = True
+
+        self.screen.blit(self.background, (0,0))
+
+        self.create_pipes()
+        self.state = create_state(bird = self.bird, pipes_group = self.pipes_group)
+
+        if action:
+            self.t=0
+            self.bird.jump_velocity = self.jump_velocity
+            self.bird.start_y = self.bird.bird_rect.y
+
+        pipe_rect_list = convert_pipes_group_to_pipe_rect(pipe_group = self.pipes_group)
+        self.die = self.bird.die(list_of_rects = pipe_rect_list)
+
+        if self.die:
+            self.restart_game()
+        else:
+            self.continue_game()
+
+        pygame.display.update()
+
+        pygame.time.wait(20)
+        
+
+    def create_pipes(self):
+        if len(self.pipes_group)==0:
+            gap_height = np.random.uniform(0.2, 0.8)
+            Pipes(gap_width = self.gap_width, pipe_width = self.pipe_width, screen_x = self.screen_x, screen_y = self.screen_y).add(self.pipes_group)
+            self.pipes_group.sprites()[0].create_pipes(x_pos = self.screen_x, gap_height = gap_height)
+        else:
+            max_index = len(self.pipes_group.sprites())-1
+            new_pipe_x = self.screen_x - int(np.round(self.pipes_group.sprites()[max_index].pipe_width * self.space_between_pipes, 0))
+            if self.pipes_group.sprites()[max_index].bot_pipe_rect.x == new_pipe_x:
+                gap_height = np.random.uniform(0.2, 0.8)
+                Pipes(gap_width = self.gap_width, pipe_width = self.pipe_width, screen_x = self.screen_x, screen_y = self.screen_y).add(self.pipes_group)
+                self.pipes_group.sprites()[max_index+1].create_pipes(x_pos = self.screen_x, gap_height = gap_height)
+            else: 
+                pass
+
+    def restart_game(self):
+        self.pipes_group.empty()
+        self.bird = Bird(screen_x = self.screen_x, screen_y = self.screen_y, gravity = 1.2)
+        self.bird.create_bird_rect(start_x = 0.5, start_y = 0.2, size_x = 44, size_y = 30)
+        self.t=0
+        self.score=0
+        self.distance = 0
+        self.reward = 0
+    
+    def continue_game(self):
+        self.reward = 1
+        self.bird.move(t = self.t)
+        self.bird.draw_bird(surface = self.screen)
+
+        for pipe in self.pipes_group:
+            pipe.remove()
+            pipe.move_pipes()
+            pipe.draw_pipes(surface=self.screen)
+        
+        self.next_state = create_state(bird = self.bird, pipes_group = self.pipes_group)
+        self.memory_frag = create_memory(state = self.state, action = self.action, next_state = self.next_state, reward = self.reward)
+  
+        self.score = self.scoring.new_score(bird = self.bird, pipes_group = self.pipes_group, score = self.score)
+        self.scoring.display_score(score = self.score, surface = self.screen)
+
+    def get_memory(self):
+        return self.memory_frag
--- main_ai.py
+++ main_ai.py
@@ -1,96 +1,11 @@
-import pygame, sys
-import numpy as np
-from game_elements import Pipes, Bird
-from game_mechanics import Score, BirdPipeRelation, convert_pipes_group_to_pipe_rect, convert_pipes_group_to_pipe_rect_pairs
-from game_memory import create_state, create_memory
-import time
+from flappybird import FlappyBird
+import random
 
-pygame.init()
-
-red = [255, 0, 0]
-background = pygame.image.load('./images/background.jpg')
-screen_x = 640
-screen_y = 480
-gap_width = 0.3
-pipe_width = 0.1
-space_between_pipes = 3 # a multiple of the pipe width
-screen = pygame.display.set_mode((screen_x, screen_y))
-pipes_group = pygame.sprite.Group()
-bird = Bird(screen_x = screen_x, screen_y = screen_y, gravity = 1.15)
-bird.create_bird_rect(start_x = 0.5, start_y = 0.2, size_x = 44, size_y = 30)
-t=0
-jump_velocity = -14
-score = 0
-scoring = Score()
-
+game = FlappyBird()
+counter = 0
 while True:
-    t = t+1
-    action = 0
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                t=0
-                bird.jump_velocity = jump_velocity
-                bird.start_y = bird.bird_rect.y
-                action = 1
-
-    screen.blit(background, (0,0))
-
-    # create new pipes if necessary
-    if len(pipes_group)==0:
-        gap_height = np.random.uniform(0.2, 0.8)
-        Pipes(gap_width = gap_width, pipe_width = pipe_width, screen_x = screen_x, screen_y = screen_y).add(pipes_group)
-        pipes_group.sprites()[0].create_pipes(x_pos = screen_x, gap_height = gap_height)
-    else:
-        max_index = len(pipes_group.sprites())-1
-        new_pipe_x = screen_x - int(np.round(pipes_group.sprites()[max_index].pipe_width * space_between_pipes, 0))
-        if pipes_group.sprites()[max_index].bot_pipe_rect.x == new_pipe_x:
-            gap_height = np.random.uniform(0.2, 0.8)
-            Pipes(gap_width = gap_width, pipe_width = pipe_width, screen_x = screen_x, screen_y = screen_y).add(pipes_group)
-            pipes_group.sprites()[max_index+1].create_pipes(x_pos = screen_x, gap_height = gap_height)
-        else: 
-            pass
-    
-    # record start state
-    state = create_state(bird = bird, pipes_group = pipes_group)
-
-    # take an action
-
-
-
-    # check if the bird died
-    pipe_rect_list = convert_pipes_group_to_pipe_rect(pipe_group = pipes_group)
-    die = bird.die(list_of_rects = pipe_rect_list)
-
-    if die:
-        # if bird dies, then restart the game
-        pipes_group.empty()
-        bird = Bird(screen_x = screen_x, screen_y = screen_y, gravity = 1.2)
-        bird.create_bird_rect(start_x = 0.5, start_y = 0.2, size_x = 44, size_y = 30)
-        t=0
-        score=0
-        distance = 0
-        reward = 0
-    else:
-        # if bird lives, then move the bird and pipes to the next location, increase the score if needed
-        reward = 1
-        bird.move(t = t)
-        bird.draw_bird(surface = screen)
-
-        for pipe in pipes_group:
-            pipe.remove()
-            pipe.move_pipes()
-            pipe.draw_pipes(surface=screen)
-        
-        next_state = create_state(bird = bird, pipes_group = pipes_group)
-        memory_frag = create_memory(state = state, action = action, next_state = next_state, reward = reward)
-  
-        score = scoring.new_score(bird = bird, pipes_group = pipes_group, score = score)
-        scoring.display_score(score = score, surface = screen)
-
-    pygame.display.update()
-
-    pygame.time.wait(20)
+    counter = counter + 1
+    action = None
+    if counter % 10 == 0:
+        action = random.sample(['True', 'False'], k = 1)[0]
+    game.run_frame(action=action)
